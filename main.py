22d7609d4b05970f9df1d45a8dbfaebb4c02c9d9
Daniel Becerra <daniel.becerra@estudiants.urv.cat>
2023-03-06 18:52:42 +0100

    commit 2

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from weatherData import WeatherData
+from meteo_utils import MeteoDataProcessor
 
 if __name__ == '__main__':
     weather_data = WeatherData()
+    processor = MeteoDataProcessor()
 
     # Generar 5 registros de datos de tiempo
+
     for i in range(5):
         data = weather_data.generate_data()
-        print(f"Registro {i+1}: Temperatura {data}")
+        print(f"Registro {i+1}: Temperatura {data.temperature}")
+        wellness_data = processor.process_meteo_data(data)
+        
+
+    
+    
+    
--- weatherData.py
+++ weatherData.py
@@ -1,8 +1,12 @@
 from meteo_utils import MeteoDataDetector
-
+from datetime import datetime
 class WeatherData:
     def __init__(self):
         self.detector = MeteoDataDetector()
 
     def generate_data(self):
-        return self.detector.gen_temperature()
+        temperature = self.detector.gen_temperature()
+        humidity = self.detector.gen_humidity()
+        timestamp = datetime.now()
+        meteodata = MeteoDataDetector(temperature=temperature, humidity=humidity, timestamp=timestamp)
+        return meteodata
