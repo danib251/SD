5a2156d7ecb10ce26d92a84b2649c2e181caedeb
Pau Pais Bertomeu <pau.pais@estudiants.urv.cat>
2023-04-04 18:40:00 +0200

    funciona per mac

--- part2/main2.py
+++ part2/main2.py
@@ -6,15 +6,18 @@ import os
 num_sensores = int(input("Ingrese la cantidad de sensores que desea ejecutar: ")) #un 2 per exemple
 # Solicitar la cantidad de terminales que el usuario desea ejecutar
 num_terminales = int(input("Ingrese la cantidad de terminales que desea ejecutar: ")) # un 3 per exemple necesites tot lo del redis
-
 # Ejecutar los archivos "sensor.py" y "rabbitserver.py"
 
 if os.name == 'nt':  # Windows
     os.system(f'start cmd.exe /c "python rabbitserver.py {"sensor_data"}"')
     os.system(f'start cmd.exe /c "python RedisDataProxy.py {num_terminales}"')
 elif os.name == 'posix':  # Linux o macOS
-    os.system(f'gnome-terminal -- python rabbitserver.py {"sensor_data"}')
-    os.system(f'gnome-terminal -- python RedisDataProxy.py {num_terminales}')
+    current_dir = os.getcwd()
+
+    #os.system(f'gnome-terminal -- python rabbitserver.py {"sensor_data"}')
+    #os.system(f'gnome-terminal -- python RedisDataProxy.py {num_terminales}')
+    os.system(f'osascript -e \'tell app "Terminal" to do script "cd {current_dir} && python rabbitserver.py sensor_data"\'')
+    os.system(f'osascript -e \'tell app "Terminal" to do script "cd ~/Documents/Quart/SD/SD/part2 && python RedisDataProxy.py {num_terminales}"\'')
 else:
     print("Sistema operativo no compatible")
 
